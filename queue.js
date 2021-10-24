f82d99a1e354f2dc522f119f429cd7b8623ca576
S-MW <72529306+S-MW@users.noreply.github.com>
2021-10-24 13:29:56 +0300

    Slove Queue

---
+++ queue.js
@@ -0,0 +1,29 @@
+class queue {
+
+    constructor(){
+        this.data = [10,20,30,40,50];
+    }
+
+    enqueue(element)
+    {
+        this.data.unshift(element)
+        // console.log(this.data)
+    }
+
+    dequeue()
+    {
+        this.data.pop()
+    }
+
+    traverse()
+    {
+        this.data.forEach(function(element){
+            console.log(element)
+        })
+    }
+}
+
+let obj = new queue()
+
+
+
