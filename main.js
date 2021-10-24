4dc9a8b4f9182bb72b7024d4bb17c0b238f55d85
Abdulmajeed Alduaifi <438101329@student.ksu.edu.sa>
2021-10-24 15:43:02 +0300

    LAB IS DONE

--- main.js
+++ main.js
@@ -34,3 +34,37 @@ class stack {
   }
 }
 //---------------------TREE
+function Node(value) {
+  this.value = value;
+  this.children = [];
+}
+
+// Create elements
+const root = new Node(0);
+
+const child1 = new Node(1);
+root.children.push(child1);
+
+const child2 = new Node(2);
+root.children.push(child2);
+
+const child3 = new Node(3);
+root.children.push(child3);
+
+const child11 = new Node(11);
+child1.children.push(child11);
+
+const child12 = new Node(12);
+child1.children.push(child12);
+
+const child31 = new Node(31);
+child3.children.push(child31);
+
+let traverse = (root) => {
+  console.log(root.value);
+  if (root.children.length > 0) {
+    root.children.forEach((el) => {
+      traverse(el);
+    });
+  }
+};
